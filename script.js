18b93ba10763e342996ee378142757191a9fcc23
samuelhenrique1234-ops <samuel_moraes@estudante.sesisenai.org.br>
2025-04-16 10:13:20 -0300

    ADD - consts declaradas

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 document.getElementById('ficha_rpg').addEventListener('submit', function (e) {
     e.preventDefault();
 
-    const nomeC = document.getElementById('nomeCliente');
-    const telefone = document.getElementById('telefone');
+    const nomeC = document.getElementById('nomeC');
     const email = document.getElementById('email');
-    const veiculoAno = document.getElementById('veiculoAno');
+    const telefone = document.getElementById('telefone');
+    const nomeP = document.getElementById('nomeP');
+    const altura = document.getElementById('altura');
+    const data_nascimento = document.getElementById('data_nascimento');
+    const masculino = document.getElementById('masculino');
+    const feminino = document.getElementById('feminino');
+    const outro = document.getElementById('outro');
+    const humano = document.getElementById('humano');
+    const demonio = document.getElementById('demonio');
+    const hibrido = document.getElementById('hibrido');
+    const combatente = document.getElementById('cobatente');
+    const alvejador = document.getElementById('alvejador');
+    const tecnico_campo = document.getElementById('tecnico_campo');
+    const infernal = document.getElementById('infernal');
+    const personagem_ft = document.getElementById('personagem_ft');
+    const lore = document.getElementById('lore');
 
 });
