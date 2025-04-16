49b862c88337d722dfed4cff6164b523669158ba
samuelhenrique1234-ops <samuel_moraes@estudante.sesisenai.org.br>
2025-04-16 10:01:02 -0300

    ADD - ajustes

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 document.getElementById('ficha_rpg').addEventListener('submit', function (e) {
     e.preventDefault();
 
-    
+    const nomeC = document.getElementById('nomeCliente');
+    const telefone = document.getElementById('telefone');
+    const email = document.getElementById('email');
+    const veiculoAno = document.getElementById('veiculoAno');
 
-}
+});
